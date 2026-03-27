39603f08081464c56b4b8ab0fe08b3bf8b370516
ChineduVickreg <nwachukwuvictorc@gmail.com>
2026-03-27 17:29:55 +0100

    Chinedu: committed GET active, and single id

--- app.js
+++ app.js
@@ -12,10 +12,43 @@ app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
 
+// GET SINGLE – Read by ID
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+
+  if (!todo) {
+    return res.status(404).json({ error: 'Todo not found' });
+  }
+
+  res.status(200).json(todo);
+});
+
+
+// GET ACTIVE TODOS (bonus)
+app.get('/todos/active', (req, res) => {
+  const activeTodos = todos.filter((t) => !t.completed);
+  res.status(200).json(activeTodos);
+});
+
+
+
 // POST New – Create
 app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+  //const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+
+  // Validation
+  if (!task) {
+    return res.status(400).json({ error: '"task" field is required' });
+  }
+
+  const newTodo = {
+    id: todos.length + 1,
+    task,
+    completed: completed ?? false, // default false if not provided
+  };
+
   todos.push(newTodo);
+  //todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
 });
 
